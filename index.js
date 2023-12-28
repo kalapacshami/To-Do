33f60f7e2380e3ba82fa27b55db4f1f72afeaacd
HammerM <marcell.hammer@gmail.com>
2023-12-28 22:43:42 +0100

    Done

--- index.js
+++ index.js
@@ -3,9 +3,29 @@ let btn= document.querySelector("#add-new-btn");
 let input = document.querySelector("#new-to-do-input");
 
 
-btn.addEventListener('click',function (){
+btn.addEventListener('click', cliclHandler)
 
-    let li= document.createElement('li')
-    li.innerHTML = input.value
-    ul.appendChild(li)
-})
+input.addEventListener('keypress',cliclHandler)
+
+    
+function cliclHandler(e){
+    
+    if(e.type === 'click' || e.key==='Enter'){
+        if(input.value!== ''){
+            let li= document.createElement('li')
+            li.innerHTML = input.value
+            ul.appendChild(li)
+
+            li.addEventListener('click', function(){
+            this.classList.toggle('ready')
+            })
+                input.value='';
+
+            }
+
+    }
+    
+}
+
+
+
